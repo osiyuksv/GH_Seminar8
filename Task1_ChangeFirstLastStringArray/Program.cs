a4d47d9e2da5e005aabf2c1dac436efb2f9fb20e
Sergey Osiyuk <osiyuksv@mail.ru>
2023-01-03 13:13:35 +0300

    Task1_ChangeFirstLastStringArray + Method

--- Task1_ChangeFirstLastStringArray/Program.cs
+++ Task1_ChangeFirstLastStringArray/Program.cs
@@ -6,38 +6,47 @@ Console.WriteLine("Задайте число колонок массива n");
 int columns = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine();
 
-int[,] matrix = new int[rows, columns];
-FillMatrixRandomNunmbers(matrix);
-
-void FillMatrixRandomNunmbers( int[,] matrix0)
+int[,] FillMatrixRandomNunmbers()
 {
+  int[,] matrix0 = new int[rows, columns];
+
   for (int i = 0; i < matrix0.GetLength(0); i++)
   {
     for (int j = 0; j < matrix0.GetLength(1); j++)
     {
       matrix0[i, j] = new Random().Next(10);
+    }
+  }
+  return(matrix0);
+}
+
+int[,] ChangeFirstLastRows(int[,] matrix0)
+{
+    for (int j = 0; j < matrix0.GetLength(1); j++)
+    {
+      int tempFirst =  matrix0[0, j];
+      matrix0[0, j] = matrix0[matrix0.GetLength(0)-1, j];
+      matrix0[matrix0.GetLength(0)-1, j] = tempFirst;
+    }
+   return(matrix0);
+}
+
+void PrintMatrix(int[,] matrix0)
+{
+  for (int i = 0; i < matrix0.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix0.GetLength(1); j++)
+    {
       Console.Write(matrix0[i, j] + " ");
     }
     Console.WriteLine();
   }
 }
 
+int[,] matrix = FillMatrixRandomNunmbers();
+PrintMatrix(matrix);
 Console.WriteLine();
 
-  for (int i = 0; i < matrix.GetLength(0); i++)
-  {
-    for (int j = 0; j < matrix.GetLength(1); j++)
-    {
-      if( i == 0 ) 
-      {
-         int tempFirst =  matrix[i, j];
-         matrix[i, j] = matrix[matrix.GetLength(0)-1, j];
-         matrix[matrix.GetLength(0)-1, j] = tempFirst;
-      }
-
-    Console.Write(matrix[i, j] + " ");
-    }
-    Console.WriteLine();
-  }
-
-
+ChangeFirstLastRows(matrix);
+PrintMatrix(matrix);
+Console.WriteLine();
